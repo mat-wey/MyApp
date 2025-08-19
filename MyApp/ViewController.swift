209372b172d88652a0d3477a472f3bb42f689966
Practicum <practicum@ya.ru>
2025-08-19 19:22:35 +0300

    Moved the appendLog function to another file

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         removeFile.layer.cornerRadius = 25
+        
     }
     
     let fileManager = FileManager.default
@@ -24,37 +25,32 @@ class ViewController: UIViewController {
     }()
     lazy var myFile: URL = documentsURL.appendingPathComponent("example.txt")
     
-    func appendLog(_ text: String) {
-        textField.text += text + "\n"
-        let range = NSMakeRange(textField.text.count - 1, 1)
-        textField.scrollRangeToVisible(range)
-    }
     
     @IBAction func removeFile(_ sender: Any) {
         if fileManager.fileExists(atPath: myFile.path) {
             try? fileManager.removeItem(at: myFile)
-            appendLog("-deleted the file")
+            appendLog(to: textField, "-deleted the file")
         } else {
-            appendLog("-The file does not exist")
+            appendLog(to: textField, "-The file does not exist")
         }
     }
     
     @IBAction func availabilityFile(_ sender: Any) {
         if fileManager.fileExists(atPath: myFile.path) {
-            appendLog("-the file is available")
-            appendLog("-true")
+            appendLog(to: textField, "-the file is available")
+            appendLog(to: textField, "-true")
         } else {
-            appendLog("-the file is not available")
-            appendLog("-false")
+            appendLog(to: textField, "-the file is not available")
+            appendLog(to: textField, "-false")
         }
     }
     
     @IBAction func addFile(_ sender: Any) {
         if fileManager.fileExists(atPath: myFile.path) {
-            appendLog("-the file already exists")
+            appendLog(to: textField,"-the file already exists")
         } else {
             fileManager.createFile(atPath: myFile.path, contents: nil)
-            appendLog("-the file is created")
+           appendLog(to: textField, "-file created")
         }
     }
 }
